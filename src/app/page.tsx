21ed4dcae0da16cba8d0ffd2ea5b8dcd5278975d
hoangtrung <trungnh@solashi.com>
2024-03-05 23:45:26 +0700

    chore: shadcn-ui, tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import styles from './page.module.css'
+import Image from 'next/image'
+import MemeImage from '../../public/meme.jpeg'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      Hi, I&apos;m Trung. I&apos;m a software engineer.
+    <main className="container min-h-screen mx-auto flex flex-col items-center justify-center">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-8">
+        Hi, I&apos;m Trung. I&apos;m a ...
+      </h1>
+
+      <Image src={MemeImage} priority alt="Pull stack developer" />
     </main>
   )
 }
